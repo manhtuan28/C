43a7aba07e727b5352ea2339281e0538ca903fea
Manh Tuan <buimanhtuan2k4@gmail.com>
2023-02-15 08:45:07 +0700

    Upload Code C

--- Chuong 1/Bai 2/Cau 4.cpp
+++ Chuong 1/Bai 2/Cau 4.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 #include <math.h>
 
-#define p 1.5
+#define p 0.5
 
 int main()
 {
@@ -18,13 +18,10 @@ int main()
 	S = sqrt(p * (p - a) * (p - b) * p - c);
 
 	if ((a + b) > c && (a + c) > b && (b + c) > a)
-	{
 		printf("Day la 3 canh cua tam giac");
-	}
 	else
-	{
 		printf("Dien tich cua tam giac la: %lf", S);
-	}
+	
 
 	getch();
 	return 0;
