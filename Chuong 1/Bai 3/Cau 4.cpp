f67f9946cc2ea7b7f5dc0a19c6aa4acdc62c05f0
manhtuan28 <buimanhtuan2k4@gmail.com>
2023-02-17 17:11:30 +0700

    Upload Code C

---
+++ Chuong 1/Bai 3/Cau 4.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+    int n;
+    int count = 0;
+    printf("Nhap vao n: ");
+    scanf("%d", &n);
+
+    if (n < 2)
+    {
+        printf("%d khong phai so nguyen to", n);
+        getch();
+        return 0;
+    }
+
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if (count == 0)
+    {
+        printf("%d la so nguyen to", n);
+    }
+    else
+    {
+        printf("%d khong pha la so nguyen to", n);
+    }
+
+    getch();
+    return 0;
+}
---
+++ Chuong 1/Bai 3/Cau 5.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k, product, sum;
+
+    printf("Cac so nguyen gom 3 chu so co tich bang tong cac chu so la:\n");
+
+    for (i = 1; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
+            for (k = 0; k <= 9; k++) {
+                product = i * j * k;
+                sum = i + j + k;
+                if (product == sum && i != j && j != k && k != i) {
+                    printf("%d%d%d\n", i, j, k);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
