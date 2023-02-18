652d0b090b6b8f691bc2b303de9bbb7d726dad70
manhtuan28 <buimanhtuan2k4@gmail.com>
2023-02-18 12:31:53 +0700

    Upload Code C

---
+++ Chuong 2/Bai 4/Cau 1.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+void tinh(){
+    int n;
+    int count = 0;
+    printf("Nhap vao n: ");
+    scanf("%d", &n);
+
+    if (n < 2)
+    {
+        printf("%d khong phai so nguyen to", n);
+    }
+
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if (count == 0)
+    {
+        printf("%d la so nguyen to", n);
+    }
+    else
+    {
+        printf("%d khong pha la so nguyen to", n);
+    }
+
+}
+
+int main () {
+    tinh();
+
+
+
+    return 0;
+}
