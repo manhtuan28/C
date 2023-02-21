f08603646ac5bb46a5063822fd4f711a1f728151
manhtuan28 <buimanhtuan2k4@gmail.com>
2023-02-21 16:06:52 +0700

    Upload Code C

---
+++ Chuong 2/Cau 4.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*
+ int giaithua;
+    gan s = 1;
+    neu n == 0 thi return 1 => printf("1");
+
+    con ko thi lap:
+    gan i = 1; i nho hon || == n; cong i them 1;
+    cho s * i roi gan lai cho s; sau do return ve s;
+
+    main:
+    int n;
+    lam: cho nguoi dung nhap vao;
+    lap lai khi n < 0;
+
+    in ra net qua giai thua N! = %d, goi ham giaithua(n);
+*/
+
+int giaithua(int n)
+{
+    int s = 1;
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        s *= i;
+    }
+    return s;
+}
+
+int main()
+{
+    int n;
+
+    do
+    {
+        printf("Nhap n: ");
+        scanf("%d", &n);
+    }
+
+    while (n < 0);
+
+    printf("N! = %d", giaithua(n));
+
+    return 0;
+}
