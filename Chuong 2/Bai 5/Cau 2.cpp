112da38a1d0c27b9dcbea0e2222da66adc87c208
manhtuan28 <buimanhtuan2k4@gmail.com>
2023-02-21 19:09:45 +0700

    Upload Code C

--- Chuong 2/Bai 5/Cau 2.cpp
+++ Chuong 2/Bai 5/Cau 2.cpp
@@ -1,1 +1,39 @@
-// Chưa làm đươccc:((()))
+#include <stdio.h>
+
+int timucln(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+    else if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        if (a > b)
+        {
+            return timucln(a - b, b);
+        }
+        else
+        {
+            return timucln(a, b - a);
+        }
+    }
+}
+
+int main()
+{
+    int a, b;
+    printf(">> Nhap vao so a: ");
+    scanf("%d", &a);
+    printf(">> Nhap vao so b: ");
+    scanf("%d", &b);
+
+    int ucln = timucln(a, b);
+
+    printf("Uoc chung lon nhat cua %d va %d la: %d\n", a, b, ucln);
+
+    return 0;
+}
--- Chuong 2/Bai 5/Cau 3.cpp
+++ Chuong 2/Bai 5/Cau 3.cpp
@@ -1,1 +1,23 @@
-// Chưa làm đươccc:((()))
+#include <stdio.h>
+
+int tong(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n + tong(n - 1);
+    }
+}
+
+int main()
+{
+    int n, sum;
+    printf("Nhap vao so n: ");
+    scanf("%d", &n);
+    sum = tong(n);
+    printf("Tong S = 1+2+3+...+%d la: %d", n, sum);
+    return 0;
+}
