1eb9abf7e29d713af9680c26326a4937d085456f
manhtuan28 <buimanhtuan2k4@gmail.com>
2023-03-06 18:35:13 +0700

    Upload Code C

---
+++ Bài tập về nhà/Cau1.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define Tuancute 100
+
+void nhap_so(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+}
+
+void hien_thi(int a[], int n)
+{
+    printf("Cac so co trong mang la: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%6d", a[i]);
+    }
+}
+
+void them_phan_tu(int a[], int n, int k, int x)
+{
+    if (k < 0 || k >= n)
+    {
+        printf("Vi tri %d khong hop le", n);
+        return;
+    }
+
+    for (int i = n - 1; i >= k; i--)
+    {
+        a[i + 1] = a[i];
+    }
+    a[k] = x;
+    n++;
+    printf("\nMang sau khi duoc them: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%6d", a[i]);
+    }
+}
+
+int main()
+{
+    int a[Tuancute];
+    int n, k, x;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    nhap_so(a, n);
+    hien_thi(a, n);
+
+    printf("\nNhap vao vi tri can doi: ");
+    scanf("%d", &k);
+    printf("Nhap gia tri: ");
+    scanf("%d", &x);
+    them_phan_tu(a, n, k, x);
+
+    return 0;
+}
---
+++ Bài tập về nhà/Cau2.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define Tuancute 100
+
+void nhap_so(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+}
+
+void hien_thi(int a[], int n)
+{
+    printf("Cac so co trong mang la: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%6d", a[i]);
+    }
+}
+
+void xoa_phan_tu(int a[], int n, int k)
+{
+    if (k < 0 || k >= n)
+    {
+        printf("Vi tri %d khong hop le", n);
+        return;
+    }
+
+    for (int i = k; i < n - 1; i++)
+    {
+        a[i] = a[i + 1];
+    }
+    n--;
+    printf("\nHam sau khi duoc xoa la: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%6d", a[i]);
+    }
+}
+
+int main()
+{
+    int a[Tuancute];
+    int n, k;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    nhap_so(a, n);
+    hien_thi(a, n);
+
+    printf("\nNhap vao vi tri can xoa: ");
+    scanf("%d", &k);
+    xoa_phan_tu(a, n, k);
+
+    return 0;
+}
