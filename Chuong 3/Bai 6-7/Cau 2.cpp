fd2f266b1e77d74d4aae3a64c41e2a184234c7e1
manhtuan28 <buimanhtuan2k4@gmail.com>
2023-03-17 12:04:34 +0700

    Upload Code C

--- Chuong 3/Bai 6-7/Cau 2.cpp
+++ Chuong 3/Bai 6-7/Cau 2.cpp
@@ -1,80 +1,117 @@
 #include <stdio.h>
 
-#define Tuancute 100
+#define MAX_ARR 100
 
-void nhap_so (float a[], int n) {
-	for(int i = 0; i < n; i++) {
-		printf("a[%d]: ", i+1);
+void nhap_mang(float a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d]: ", i);
 		scanf("%f", &a[i]);
 	}
 }
 
-
-void hien_thi (float a[], int n) {
-	printf("-Cac so vua nhap trong mang la: ");
-	for(int i = 0; i < n; i++) {
+void hien_thi(float a[], int n)
+{
+	printf("\n>> Cac so co trong mang la: ");
+	for (int i = 0; i < n; i++)
+	{
 		printf("%g  ", a[i]);
 	}
 }
 
-float tinh_tong (float a[], int n) {
+float tinh_tong(float a[], int n)
+{
 	float sum = 0;
-	for(int i = 0; i < n; i++) {
-		if(a[i] >= -10 && a[i] <= 20) {
-			sum+=a[i];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] >= -10 && a[i] <= 20)
+		{
+			sum += a[i];
 		}
 	}
 	return sum;
 }
 
-float tinh_tbc (float a[], int n) {
-	int dem = 0;
-	for(int i = 0; i < n; i++) {
-		if(a[i] >= -10 && a[i] <= 20) {
+float tinh_tbc(float a[], int n)
+{
+	float dem = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] >= -10 && a[i] <= 20)
+		{
 			dem++;
 		}
 	}
-	int tbc = tinh_tong(a,n) / dem;
-	return tbc;
+	if (dem == 0)
+	{
+		return 0;
+	}
+	float tbc = tinh_tong(a, n) / dem;
+	return dem;
 }
 
-int so_lon_nhat (float a[], int n) {
-	int max = a[0];
-	for(int i = 0; i < n; i++) {
-		if(a[i] > max) {
+float so_lon_nhat(float a[], int n)
+{
+	float max = a[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > max)
+		{
 			max = a[i];
 		}
 	}
 	return max;
 }
 
-void chen_so(float a[], int n, float k, int m) {
-	printf("\n\nNhap gia tri k can chen: ");
-	scanf("%f", &k);
-	printf("Nhap vi tri m can chen k vao: ");
+void them_phan_tu(float a[], int n)
+{
+	int m;
+	float k;
+	printf("\n\nNhap vao vi tri can them: ");
 	scanf("%d", &m);
-	for (int i = n - 1; i >= m; i--) {
+	printf("Nhap vao gia tri can them: ");
+	scanf("%f", &k);
+
+	if (m < 0 || m > n)
+	{
+		printf("Vi tri %d khong hop le", m);
+		return;
+	}
+
+	for (int i = k; i >= m; i--)
+	{
 		a[i + 1] = a[i];
 	}
-	a[k] = m;
+	a[m] = k;
 	n++;
-	printf("-Day so sau khi chen k vao vi tri m:\n");
-	hien_thi(a, n + 1);
+	printf("\n>> Sau khi them phan tu: ");
+	hien_thi(a, n);
 }
 
-int main () {
-	float a[Tuancute],k;	
-	int n,m;
-	printf("Nhap vao so luong so thuc: ");
+int main()
+{
+	float a[MAX_ARR];
+	int n;
+	printf("Nhap vao so n: ");
 	scanf("%d", &n);
 
-	nhap_so(a,n);
-	hien_thi(a,n);
-	printf("\n-Tong cac so trong khoang [-10, 20] la: %g", tinh_tong(a,n));
-	printf("\n-Trung binh cong cac so trong khoang [-10, 20] la: %g", tinh_tbc(a,n));
-	printf("\n-So lon nhat tinh theo so nguyen la: %d", so_lon_nhat(a,n));
+	nhap_mang(a, n);
+	hien_thi(a, n);
 
-	chen_so(a, n, k, m);
+	if (tinh_tong(a, n) == 0)
+		printf("\n>> Khong co so nao trong khoang [-10,20]");
+	else
+		printf("\n>> Tong trong khoang [-10,20] la: %g", tinh_tong(a, n));
+
+	if (tinh_tbc(a, n) == 0)
+		printf("\n>> Khong co so nao trong khoang [-10,20]");
+	else
+		printf("\n>> Trung binh cong trong khoang [-10,40] la: %g", tinh_tbc(a, n));
+
+	printf("\n>> So lon nhat trong mang la: %g", so_lon_nhat(a, n));
+
+	them_phan_tu(a, n);
 
 	return 0;
 }
